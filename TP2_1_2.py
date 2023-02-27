688b761200df1aca9679d91148653cdaa1aa1e21
TW-Arzer <115579050+TW-Arzer@users.noreply.github.com>
2023-02-27 09:34:40 +0100

    Update TP2_1_2.py

--- TP2_1_2.py
+++ TP2_1_2.py
@@ -2,9 +2,14 @@ import sys
 from functools import reduce
 
 list = sys.argv[1:]
+int_list = []
+
 
 if len(list) > 0:
-	result = reduce(lambda x, y: x * y, list)
+	for i in list:
+		new_i = int(i)
+		int_list.append(new_i)
+	result = reduce(lambda x, y: x * y, int_list)
 	print(result)
 else:
 	print(None)
