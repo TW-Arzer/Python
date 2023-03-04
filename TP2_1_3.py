c0e21d45410db27a2b3015cdebd061e888784ec6
TW-Arzer <115579050+TW-Arzer@users.noreply.github.com>
2023-03-04 08:55:04 +0100

    Update TP2_1_3.py

--- TP2_1_3.py
+++ TP2_1_3.py
@@ -6,13 +6,14 @@ int_list = []
 
 
 for i in list:
-	new_i = int(i)
-	int_list.append(new_i)
-	if int_list[1] == 0:
-		raise ZeroDivisionError("Can't divide by 0")
-	if len(list) > 0:		
-		result = reduce(lambda x, y: x / y, int_list)
-		print(result)
-	else:
-		print(None)
-	
+    new_i = int(i)
+    int_list.append(new_i)
+
+try:
+    result = reduce(lambda x, y: x / y, int_list)
+except ZeroDivisionError as e:
+    print("Division by 0 not defined")
+except TypeError as e:
+    print("None")
+else:
+    print(result)
