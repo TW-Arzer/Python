684c33ac193e54df4c198abd7ca4a649a781bf71
TW-Arzer <115579050+TW-Arzer@users.noreply.github.com>
2023-03-06 09:41:12 +0100

    Delete Python directory

--- Python/TP2_1_1.py
+++
@@ -1,9 +0,0 @@
-
-def echo(x):
-    print(x)
-
-echo("Buechstabe")
-
-if __name__ == "__main__":
-    print("es paar Buechstabe")
-
--- Python/TP2_1_1_2.py
+++
@@ -1,4 +0,0 @@
-from TP2_1_1 import echo
-
-if __name__ == "__main__":
-    echo("nomol paar Buechstabe")
